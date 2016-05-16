9c1721b8e3f6c0d475c761a9cf7d2e8efbbc2560
wangshiyu13 <wangshiyu13@163.com>
2016-05-16 23:20:32 +0800

    Initial Commit

---
+++ HLSocial/HLSocial.h
@@ -0,0 +1,19 @@
+//
+//  HLSocial.h
+//  HLSocial
+//
+//  Created by wangshiyu13 on 16/5/16.
+//  Copyright © 2016年 wangshiyu13. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for HLSocial.
+FOUNDATION_EXPORT double HLSocialVersionNumber;
+
+//! Project version string for HLSocial.
+FOUNDATION_EXPORT const unsigned char HLSocialVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <HLSocial/PublicHeader.h>
+
+
